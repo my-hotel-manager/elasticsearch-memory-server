3d83b765fd6c19e9d005c76d3928a79927a28ff4
Agni Ilango <agni@myhotelmanager.co.uk>
2020-06-15 15:04:15 +0100

    Add port as config option

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import ElasticMemoryServer from 'ElasticMemoryServer';
 
 const main = async () => {
+  // const server = new ElasticMemoryServer({ instance: { port: 9200 } });
   const server = new ElasticMemoryServer();
   await server.getUri();
   console.log(server.instanceInfoSync);
