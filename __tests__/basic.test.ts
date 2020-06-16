03c1f59fc126af8858f61ce2708b2dc1d28a121d
Naim Sen <naim@pop-os.localdomain>
2020-06-16 23:09:33 +0100

    Fix test to poll localhost for JSON response

--- __tests__/basic.test.ts
+++ __tests__/basic.test.ts
@@ -1,7 +1,25 @@
 import ElasticMemoryServer from '../index';
+import http from 'http';
 
 it('runs', async () => {
-  const server = new ElasticMemoryServer({ instance: { port: 9200 } });
+  const server = new ElasticMemoryServer({ instance: { port: 3131 } });
   const uri = await server.getUri();
-  expect(uri).toBe('http://127.0.0.1:9200');
+  http.get(uri, (res) => {
+    let data = '';
+    res.on('data', (chunk) => {
+      data += chunk;
+    });
+
+    res.on('end', () => {
+      expect(JSON.parse(data)).toMatchObject({
+        name: expect.any(String),
+        cluster_name: expect.any(String),
+        cluster_uuid: expect.any(String),
+        version: expect.any(Object)
+      });
+    });
+  }).on('error', (err) => {
+    if (err) throw err;
+  });
+  // expect(uri).toBe('http://127.0.0.1:3131');
 });
